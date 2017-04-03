d4e5415c259d2b0f899bf55f0f4c5eabb847c9d7
TortugaAttack <felixconrads@googlemail.com>
2017-04-03 14:01:42 +0200

    migrate 6

---
+++ gerbil-commons/src/main/java/org/hobbit/gerbil/commons/CONSTANTS.java
@@ -0,0 +1,18 @@
+package org.hobbit.gerbil.commons;
+
+/**
+ * Constants which are used by more than one project
+ * 
+ * @author f.conrads
+ *
+ */
+public class CONSTANTS {
+
+    public final static String GERBIL_DATA_GENERATOR_NUMBER_OF_DOCUMENTS_KEY = "gerbil-benchmark.numberOfDocuments";
+    public final static String GERBIL_DATA_GENERATOR_SEED_KEY = "gerbil-benchmark.seed";
+    
+    public final static String GERBIL_TASK_GENERATOR_EXPERIMENT_TYPE_PARAMETER_KEY = "gerbil.experiment_type";
+    
+    public final static String GERBIL_EVALUATION_MODULE_EXPERIMENT_TYPE_KEY = "gerbil.experimentType";
+
+}
