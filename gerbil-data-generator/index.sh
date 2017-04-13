238b21acf755d1dfeb37e4e3edd58438cfb10b3f
TortugaAttack <felixconrads@googlemail.com>
2017-04-13 14:50:46 +0200

    use index entity check

--- gerbil-data-generator/index.sh
+++ gerbil-data-generator/index.sh
@@ -2,3 +2,8 @@ wget http://139.18.2.164/mroeder/gerbil/dbpedia_index.zip  -P indexes/dbpedia
 cd indexes/dbpedia/
 unzip dbpedia_index.zip
 rm dbpedia_index.zip
+
+wget http://139.18.2.164/mroeder/gerbil/dbpedia_check_index.zip -P indexes/dbpedia_check
+cd indexes/dbpedia_check/
+unzip dbpedia_check_index.zip
+rm dbpedia_check_index.zip
