8e170aa24516126e68e34455a6850e6b0fad7bdd
mroeder <roeder@informatik.uni-leipzig.de>
2017-05-24 14:07:58 +0200

    added data needed by the bengal lib.

--- gerbil-data-generator/init.sh
+++ gerbil-data-generator/init.sh
@@ -1,3 +1,7 @@
 wget http://hobbitdata.informatik.uni-leipzig.de/gerbil/gerbil_data.zip
 unzip gerbil_data.zip
 rm gerbil_data.zip
+
+wget http://hobbitdata.informatik.uni-leipzig.de/bengal/en_surface_forms.tsv.zip
+unzip en_surface_forms.tsv.zip
+rm en_surface_forms.tsv.zip
